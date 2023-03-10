cf92aaae93ed1ebb9ab62952f0e677466c33ade3
Kyronne <kyronne.parkes@gmail.com>
2023-03-10 14:32:54 +0000

    Developing 3d background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import React from "react";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  Enviroment
+} from "@react-three/drei"
+
 function App(){
   return (
     <>
+    <Enviroment 
+    background={"only"}
+    files={"/assets/textures/envmap_blur_hdr"}
+    ground={{ height: 100, radius: 300}}
+    />
+    <Enviroment 
+    background={false}
+    files={"/assets/textures.envmap.hdr"}
+    />
+
+    <PerspectiveCamera makeDefault fov={33} position={[-0,09, 16.01, -27.9]}/>
+    <OrbitControls target={[0.304, 0.806, 0.427]} maxPolarAngle={Math.PI * 0.45}/>
     </>
   );
 }
